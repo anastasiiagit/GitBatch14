37563d75d456aab8d00b912e30690b1dcc788f4e
Anastasiia Balaniuk <anastasiiabalaniuk@anastasiias-air.myfiosgateway.com>
2022-09-27 20:07:32 -0400

    third commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,6 +4,8 @@ public class class03 {
 
 	public static void main(String[] args) {
 		System.out.println("Hey this is a git class");
+		System.out.println("Hey this is a git class 2");
+		System.out.println("Hey this is a git class 3");
 	}
 
 }
